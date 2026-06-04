78c654ebc73becccec74883e72691c362c5c3518
ht1345217-droid <ht1345217@gmail.com>
2026-06-04 14:11:53 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-// Redirect to frontend home page
+// Redirect to frontend home page<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 header("Location: frontend/index.php");
 exit();
 ?>
